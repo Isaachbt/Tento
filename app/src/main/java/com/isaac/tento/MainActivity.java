65950b8bfcf62f832c248169909dd8fde214249b
Isaac <isaac.silva1478@gmail.com>
2023-10-13 23:57:04 -0300

    add ponto, remove e total vitorias finalizado com sucesso

--- app/src/main/java/com/isaac/tento/MainActivity.java
+++ app/src/main/java/com/isaac/tento/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     private int pontosValendo = 1;
     private int pontosPlayer= 0;
     private int pontosAdversario = 0;
+    private int pontosMaximoVitoria = 12;//isso aqui vai mudar, vou usar  shared
+    private int totalVitoriasPlayer = 0;
+    private int totalVitoriasAdversario = 0;
 
     private ActivityMainBinding binding;
 
@@ -19,26 +22,60 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
+        addPontos();
+        removePontos();
 
     }
 
-    private void clicks()
+    private void removePontos()
+    {
+        binding.diminuirPontosPlayer.setOnClickListener(v -> {
+            pontosPlayer -= 1;
+            atualizarValoresTela();
+        });
+
+        binding.diminuirPontosAdversario.setOnClickListener(v -> {
+            pontosAdversario -= 1;
+            atualizarValoresTela();
+        });
+    }
+
+    private void addPontos()
     {
         binding.addPontosPlayer.setOnClickListener(v -> {
             pontosPlayer += pontosValendo;
-            binding.pontosPlayer.setText(pontosPlayer);
+            atualizarValoresTela();
+            verificarVitoria();
         });
 
         binding.addPontosAdversario.setOnClickListener(v -> {
             pontosAdversario += pontosValendo;
-            binding.pontosAdversario.setText(pontosAdversario);
+            atualizarValoresTela();
+            verificarVitoria();
         });
-        verificarVitoria();
+
+
     }
 
     private void verificarVitoria()
     {
+        if (pontosPlayer >= pontosMaximoVitoria) {
+            totalVitoriasPlayer += 1;
+            pontosAdversario = 0;
+            pontosPlayer = 0;
+        } else if (pontosAdversario >= pontosMaximoVitoria) {
+            totalVitoriasAdversario += 1;
+            pontosAdversario = 0;
+            pontosPlayer = 0;
+        }
+        atualizarValoresTela();
+    }
 
+    private void  atualizarValoresTela()
+    {
+        binding.pontosPlayer.setText(String.valueOf(pontosPlayer));
+        binding.pontosAdversario.setText(String.valueOf(pontosAdversario));
+        binding.vitoriaPlayer.setText(String.valueOf(totalVitoriasPlayer));
+        binding.vitoriaAdversario.setText(String.valueOf(totalVitoriasAdversario));
     }
 }
